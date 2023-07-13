f2506b7bdf24a7eaee7c6fdb0def0323e087e4ea
Rosen Yordanov <roskata182003@gmail.com>
2023-07-13 20:07:50 +0300

    Change bookingContext

--- BookingWebProject.Infrastructure/Data/BookingContext.cs
+++ BookingWebProject.Infrastructure/Data/BookingContext.cs
@@ -1,13 +1,47 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BookingWebProject.Data
+﻿namespace BookingWebProject.Data
 {
-    public class BookingContext : IdentityDbContext
+    using Infrastructure.Data.Configurations;
+    using Infrastructure.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class BookingContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public BookingContext(DbContextOptions<BookingContext> options)
             : base(options)
         {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; } = null!;
+        public DbSet<Room> Rooms { get; set; } = null!;
+        public DbSet<RoomBasis> RoomBasis { get; set; } = null!;
+        public DbSet<RoomType> RoomTypes { get; set; } = null!;
+        public DbSet<Reservation> Reservations { get; set; } = null!;
+        public DbSet<RentCar> RentCars { get; set; } = null!;
+        public DbSet<Benefit> Benefits { get; set; } = null!;
+        public DbSet<Picture> Pictures { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!;
+        public DbSet<HotelBenefits> HotelBenefits { get; set; } = null!;
+        public DbSet<RoomPackage> RoomPackages { get; set; } = null!;
+        public DbSet<RoomsBases> RoomsBases { get; set; } = null!;
+        public DbSet<FavoriteHotels> FavoriteHotels { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new HotelBenefitsConfiguration());
+            builder.ApplyConfiguration(new RoomBasisEntityConfiguration());
+            builder.ApplyConfiguration(new HotelEntityConfiguration());
+            builder.ApplyConfiguration(new PackageEntityConfiguration());
+            builder.ApplyConfiguration(new RentCarEntityConfiguration());
+            builder.ApplyConfiguration(new RoomTypeEntityConfiguration());
+            builder.ApplyConfiguration(new BenefitEntityConfiguration());
+            builder.ApplyConfiguration(new RoomEntityConfiguration());
+            builder.ApplyConfiguration(new RoomBasesConfiguration());
+            builder.ApplyConfiguration(new PictureEntityConfiguration());
+            builder.ApplyConfiguration(new FavoriteHotelsEntityConfiguration());
+            //builder.ApplyConfiguration(new CommentEntityConfiguration());
         }
     }
 }
