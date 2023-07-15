ee7e58cc0399d4a94b8a26106a77d6e367a7c54d
Rosen Yordanov <roskata182003@gmail.com>
2023-07-15 10:18:11 +0300

    Add PictureViewModel and HotelCardViewModel

---
+++ BookingWebProject.Core/Models/Hotel/HotelCardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BookingWebProject.Core.Models.Hotel
+{
+    using Models.Picture;
+    public class HotelCardViewModel
+    {
+        public HotelCardViewModel()
+        {
+            Pictures = new List<PictureViewModel>();
+        }
+        public int Id { get; init; }
+        public string Name { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public int Stars { get; set; }
+        public List<PictureViewModel> Pictures { get; set; }
+    }
+}
+
---
+++ BookingWebProject.Core/Models/Picture/PictureViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookingWebProject.Core.Models.Picture
+{
+    public class PictureViewModel
+    {
+        public string Path { get; set; } = null!;
+    }
+}
