a14c7dff98a9d24f875596668308c7b057b63af4
Rosen Yordanov <roskata182003@gmail.com>
2023-07-15 10:15:21 +0300

    Add JS Code for Register and Login pages inputs

---
+++ BookingWebProject/wwwroot/js/account.js
@@ -0,0 +1,6 @@
+﻿let inputElements = document.querySelectorAll("input").forEach((input) => {
+    if (!input.readOnly) {
+        input.addEventListener("focus", (event) => event.target.parentElement.children[0].classList.add("shift"));
+        input.addEventListener("blur", (event) => event.target.parentElement.children[0].classList.remove("shift"));
+    }
+});
