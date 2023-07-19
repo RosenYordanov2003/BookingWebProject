09e301638ed4e3118f7024c5b0b717701e0a5d45
Rosen Yordanov <roskata182003@gmail.com>
2023-07-19 13:23:26 +0300

    Add JS to RentCar Details View

---
+++ BookingWebProject/wwwroot/js/RentCar/Details.js
@@ -0,0 +1,105 @@
+﻿const makeType = document.querySelector('table tbody tr td').textContent;
+//Get url
+const url = new URL(window.location.href);
+//Get the last element from URL path
+let id = url.pathname.split('/').pop();
+const baseUrl = '/Car/CarsByBrand';
+getCarsByBrand(makeType, baseUrl, id);
+
+
+async function getCarsByBrand(brand, baseUrl, id) {
+    try {
+        const response = await fetch(baseUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `brand=${encodeURIComponent(brand)}&id=${encodeURIComponent(id)}`
+        });
+
+        if (response.ok) {
+            const data = await response.text();
+            document.getElementById("other-cars").innerHTML += data;
+            let childrenCount = document.getElementById('other-cars').children.length;
+            if (childrenCount == 0) {
+                document.querySelector('.suggest-title').style.display = 'none';
+                let [leftArrow, rightArrow] = [...document.querySelectorAll('.suggest-section button')]
+                leftArrow.style.display = 'none';
+                rightArrow.style.display = 'none';
+            }
+            else {
+                document.querySelector('.suggest-title').style.display = 'block';
+                createSlider();
+            }
+
+        } else {
+            console.error('Response error:', response.status);
+        }
+    } catch (exception) {
+        console.error(exception);
+    }
+}
+
+
+function createSlider() {
+    const initialCountCards = 2;
+    let currentIndex = 0;
+    const cards = [...document.querySelectorAll('#other-cars .card')];
+    ///Показване на първите 2 контейнера.
+    for (var index = 0; index < initialCountCards; index++) {
+        if (cards[index]) {
+            cards[index].style.display = 'flex';
+        }
+    }
+    // Взимане на лявата и дясната стрелка чрез DOM.
+    const leftArrowBtn = document.querySelector('.suggest-section .left-arrow');
+    const rightArrowBtn = document.querySelector('.suggest-section .right-arrow');
+
+    rightArrowBtn.addEventListener('click', () => {
+        //Скриване на 2 контейнра от текщия индекс(включително) до текущия индекс + 2 (не е включително)
+        for (var index = currentIndex; index < initialCountCards + currentIndex; index++) {
+            //Прави се проверка дали на този индекс съществува елемент.
+            if (cards[index]) {
+                cards[index].style.display = 'none';
+            }
+        }
+        currentIndex += initialCountCards;
+        // Ако текъщия индекс е надвишил или изравнил размера на card масива, индекса се занулява.
+        if (currentIndex >= cards.length) {
+            currentIndex = 0;
+        }
+        //Показване на елементи от текущия индекс (включително) до текущия индекс + 2 (не е включително)
+        for (var index = currentIndex; index < initialCountCards + currentIndex; index++) {
+            if (cards[index]) {
+                cards[index].style.display = 'flex';
+            }
+        }
+
+    });
+    leftArrowBtn.addEventListener('click', () => {
+        let initialIndex = currentIndex;
+
+        // Проверка дали има достатъчно контейнери преди текущия индекс, за да се покажат двата предишни контейнера.
+        if (initialIndex - initialCountCards >= 0) {
+            initialIndex -= initialCountCards;
+        } else {
+            // Ако няма достатъчно контейнери преди текущия индекс, покажете последните два контейнера.
+            initialIndex = cards.length - initialCountCards;
+        }
+
+        // Скриване на текущите контейнери.
+        for (var index = currentIndex; index < currentIndex + initialCountCards; index++) {
+            if (cards[index]) {
+                cards[index].style.display = 'none';
+            }
+        }
+
+        // Показване на предишните два контейнера.
+        for (var index = initialIndex; index < initialIndex + initialCountCards; index++) {
+            if (cards[index]) {
+                cards[index].style.display = 'flex';
+            }
+        }
+        currentIndex = initialIndex;
+    });
+}
