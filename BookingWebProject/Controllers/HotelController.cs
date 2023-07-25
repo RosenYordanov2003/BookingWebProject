e29389ade5c1832b47fa33feefee80e189e67f3b
Rosen Yordanov <roskata182003@gmail.com>
2023-07-25 18:13:59 +0300

    Add View with design to HotelController All Action and change Account Login view

--- BookingWebProject/Controllers/HotelController.cs
+++ BookingWebProject/Controllers/HotelController.cs
@@ -1,10 +1,13 @@
 ﻿namespace BookingWebProject.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Core.Contracts;
     using Core.Models.Hotel;
     using Core.Models.Pager;
     using Extensions;
+
+    [Authorize]
     public class HotelController : Controller
     {
         private readonly IBenefitService benefitService;
@@ -23,7 +26,13 @@
                 hotelQueryViewModel.CurrentPage = 1;
             }
             Guid userId = User.GetId();
-          
+            Pager pager = new Pager(await hotelService.GetCountAsync(hotelQueryViewModel), hotelQueryViewModel.CurrentPage);
+            hotelQueryViewModel.Pager = pager;
+            AllHotelsSortedAndFilteredDataModel sortedHotels = await hotelService.GetAllHotelsSortedAndFilteredAsync(userId, hotelQueryViewModel);
+            hotelQueryViewModel.HotelViewModels = sortedHotels.Hotels;
+            hotelQueryViewModel.Benefits = await benefitService.GetAllBenefitsAsync();
+            hotelQueryViewModel.Cities = await hotelService.GetAllHotelCitiesAsync();
+            hotelQueryViewModel.Countries = await hotelService.GetAllHotelCountriesAsync();
             return View(hotelQueryViewModel);
         }
     }
